90c39a08658cca916d287659d010d0deea414bb6
Mads Degn <mads.degn@outlook.dk>
2025-03-20 12:37:22 +0100

    Added comments

--- secrets.php
+++ secrets.php
@@ -1,4 +1,4 @@
-<!-- secrets -->
+<!-- Secrets -->
 
 <!-- Mads Degn -->
 <!-- 13/03-25 -->
@@ -8,40 +8,50 @@
     <head>
     <style>
 
-#title {
-    font-family: Arial, sans-serif;
-    text-align: center;
-    background-color: white;
-    width: 300px;
-    border: 7px solid black;
-    padding: 2px;
-    margin: auto;
-    font-size: 45px;
-}
+        /* CSS selector for WITSocial logo at top of webpage. */
+        /* Applies styles to the <div> with id "Title". */
+        /* Styles include a CSS Box Model with a thick black border, white background and the name WITSocial largely displayed in the middel */
+        #title {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            background-color: white;
+            width: 300px;
+            border: 7px solid black;
+            padding: 2px;
+            margin: auto;
+            font-size: 45px;
+        }
 
-#content {
-    font-family: Arial, sans-serif;
-    text-align: center;
-    background-color: white;
-    width: 400px;
-    border: 7px solid black;
-    padding: 50px;
-    margin: auto;
-    font-size: 20px;
-}
+        /* CSS selector for main content/secrets part of webpage. */
+        /* Applies styles to the <div> with id "Content". */
+        /* Styles include a CSS Box Model with a thick black border, white background and the secrets paragraph in the middel */
+        #content {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            background-color: white;
+            width: 400px;
+            border: 7px solid black;
+            padding: 50px;
+            margin: auto;
+            font-size: 20px;
+        }
 
-</style>
+    </style>
     </head>
     <body>
-<br>
+        <br>
+
+        <!-- Creates division with id title and writes WITS in capital lettes followed by ocial in non-capital letters. -->
         <div id="title">
-    <b>WITS</b>ocial
-</div>
+            <b>WITS</b>ocial
+        </div>
 
-<br><br>
+        <br><br>
 
-<div id="content">
-    Welcome to the <b>WITS</b>ocial social media platform where we apparently like to tell eachother secrets. I don't have any secrets to tell you right now, but I'm assuming someone else might. Enjoy!
-</div>
+        <!-- Creates division with id content and writes paragraph about secrets, again with WITS in capital letters -->
+        <div id="content">
+            Welcome to the <b>WITS</b>ocial social media platform where we apparently like to tell eachother secrets. I don't have any secrets to tell you right now, but I'm assuming someone else might. Enjoy!
+        </div>
+
     </body>
 </html>
